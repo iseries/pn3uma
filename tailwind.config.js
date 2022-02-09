793833a53b0c5854b7427ed66d561d3e7febb84f
Rene <contact@renerehme.de>
2022-02-09 14:59:15 +0100

    add path to check for tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	content: [
 		'./DistributionPackages/Pn3uma.App/Resources/Private/Layouts/*.html',
 		'./DistributionPackages/Pn3uma.App/Resources/Private/Templates/*/*.html',
+		'./DistributionPackages/Pn3uma.App/Resources/Public/Scripts/main.js',
 	],
 	theme: {
 		extend: {},
