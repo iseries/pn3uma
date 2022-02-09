a12df4f9e5715e7c3fb1d203e79dcbf216028fed
Rene <contact@renerehme.de>
2022-02-09 07:52:28 +0100

    Add services

---
+++ DistributionPackages/Pn3uma.App/Classes/Domain/Service/NetworkService.php
@@ -0,0 +1,74 @@
+<?php
+namespace Pn3uma\App\Domain\Service;
+
+/*
+ * This file is part of the Pn3uma.App package.
+ */
+
+use Neos\Flow\Annotations as Flow;
+
+/**
+ * The network service provides logic for getting network stuff
+ * @Flow\Scope("singleton")
+ * @api
+ */
+class NetworkService
+{
+    const NO_DOMAIN_GIVEN = false;
+    const DOMAIN_GIVEN = true;
+
+    /**
+     * check port
+     * @param string $domain
+     * @return array
+     */
+    public function checkPort(string $domain = ''): array
+    {
+        $status = NetworkService::NO_DOMAIN_GIVEN;
+        $response = 'error';
+
+        if($domain) {
+            $host = trim(str_replace('https://', '', $domain));
+            $port = 443;
+            $response = 'Port '.$port.' is closed';
+
+            $connection = @fsockopen($host, $port);
+            if (is_resource($connection)) {
+                $status = NetworkService::DOMAIN_GIVEN;
+                $response = 'Port '.$port.' is open';
+                fclose($connection);
+            }
+        }
+
+        return array(
+            'status' => $status,
+            'response' => $response
+        );
+    }
+
+    /**
+     * check http header
+     * @param string $domain
+     * @return array
+     */
+    public function checkHttpHeader(string $domain = ''): array
+    {
+        $status = NetworkService::NO_DOMAIN_GIVEN;
+        $response = 'error';
+
+        if($domain !== '') {
+            $response = 'not reachable';
+
+            $domainHeaders = @get_headers($domain);
+            if(is_array($domainHeaders)) {
+                $status = NetworkService::DOMAIN_GIVEN;
+                $response = $domainHeaders;
+            }
+        }
+
+        return array(
+            'status' => $status,
+            'response' => $response
+        );
+    }
+}
